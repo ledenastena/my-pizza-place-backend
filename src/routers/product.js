4ee156436813a3e16cf6ef43c990f64f5c7c1d69
Milan Lazic <kockastajabuka@gmail.com>
2020-10-23 15:36:48 +0200

    Edge case for adding a product without an image

--- src/routers/product.js
+++ src/routers/product.js
@@ -8,20 +8,20 @@ const productRouter = new express.Router()
 
 const upload = multer({
   limits: {
-    fileSize: 1000000
+    fileSize: 1000000,
   },
   fileFilter: function (req, file, cb) {
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
       return cb(new Error('Please provide a jpg/jpeg/png file!'))
     }
     cb(undefined, true)
-  }
+  },
 })
 
 productRouter.get('/products', async (req, res) => {
   try {
     if (req.query.type) {
-      const requestedType = await ProductType.findOne({ name: req.query.type})
+      const requestedType = await ProductType.findOne({ name: req.query.type })
 
       if (requestedType) {
         await requestedType.populate('products').execPopulate()
@@ -31,8 +31,7 @@ productRouter.get('/products', async (req, res) => {
 
     const products = await Product.find()
     res.send(products)
-  }
-  catch(err) {
+  } catch (err) {
     res.status(500).send()
   }
 })
@@ -40,60 +39,75 @@ productRouter.get('/products', async (req, res) => {
 // Url for product image to use on frontend
 productRouter.get('/products/:_id/:image_name', async (req, res) => {
   try {
-    const product = await Product.findOne({ _id: req.params._id, image_name: req.params.image_name})
+    const product = await Product.findOne({
+      _id: req.params._id,
+      image_name: req.params.image_name,
+    })
 
     if (!product || !product.image) {
       throw new Error()
     }
     res.set('Content-Type', 'image/png')
     res.send(product.image)
-  }
-  catch(err) {
+  } catch (err) {
     res.status(404).send()
   }
 })
 
-productRouter.post('/products', auth, authAdmin, upload.single('image'), async (req, res) => {
-  const product = new Product(req.body)
-  
-  try {
-    
-    if (req.file.buffer) {
-      product.image = req.file.buffer
-      product.image_name = req.file.originalname
+productRouter.post(
+  '/products',
+  auth,
+  authAdmin,
+  upload.single('image'),
+  async (req, res) => {
+    const product = new Product(req.body)
+
+    try {
+      if (req.file) {
+        product.image = req.file.buffer
+        product.image_name = req.file.originalname
+      }
+
+      await product.save()
+      res.status(201).send()
+    } catch (err) {
+      res.status(400).send('Invalid data.')
     }
-
-    await product.save()
-    res.status(201).send()
+  },
+  (error, req, res, next) => {
+    res.status(400).send('Invalid file upload.')
   }
-  catch(err) {
-    res.status(400).send('Invalid data.')
-  }
-}, (error, req, res, next) => {
-  res.status(400).send('Invalid file upload.')
-})
+)
 
-productRouter.patch('/products/:id', auth, authAdmin, upload.single('image'), async (req, res) => {
-  const _id = req.params.id
-  
-  try {
-    const updateObj = req.body
-    if (req.file) {
-      updateObj.image = req.file.buffer
-      updateObj.image_name = req.file.originalname
+productRouter.patch(
+  '/products/:id',
+  auth,
+  authAdmin,
+  upload.single('image'),
+  async (req, res) => {
+    const _id = req.params.id
+
+    try {
+      const updateObj = req.body
+      if (req.file) {
+        updateObj.image = req.file.buffer
+        updateObj.image_name = req.file.originalname
+      }
+      const product = await Product.findByIdAndUpdate(_id, updateObj, {
+        new: true,
+        runValidators: true,
+      })
+
+      if (!product) {
+        return res.status(404).send('Product not found.')
+      }
+
+      res.send(product)
+    } catch (err) {
+      res.status(400).send('Invalid data.')
     }
-    const product = await Product.findByIdAndUpdate(_id, updateObj, {new: true, runValidators: true })
-
-    if (!product) {
-      return res.status(404).send('Product not found.')
-    }
-
-    res.send(product)
   }
-  catch(err) {
-    res.status(400).send('Invalid data.')
-  }
-})
+)
 
 productRouter.delete('/products/:id', auth, authAdmin, async (req, res) => {
   const _id = req.params.id
@@ -106,8 +120,7 @@ productRouter.delete('/products/:id', auth, authAdmin, async (req, res) => {
     }
 
     res.send(product)
-  }
-  catch(err) {
+  } catch (err) {
     res.status(500).send()
   }
 })
